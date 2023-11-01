d6fe8d7a2bd8647146b46acb39234a7668717112
LuisTakeo <luistakeo@hotmail.com>
2023-11-01 19:14:00 -0300

    verificação adicional do calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tpaim-yu <tpaim-yu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:16:06 by tpaim-yu          #+#    #+#             */
-/*   Updated: 2023/10/24 15:35:37 by tpaim-yu         ###   ########.fr       */
+/*   Updated: 2023/10/31 22:33:11 by tpaim-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	total_size;
 
 	total_size = nmemb * size;
+	if (total_size / nmemb != size)
+		return (NULL);
 	ptr = malloc(total_size);
 	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
